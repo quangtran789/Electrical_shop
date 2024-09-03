461a802275b8c1a882dd299512cc916fc8f3e6b7
quangtran789 <quangvutran61@gmail.com>
2024-09-03 19:20:04 +0700

    Add project files.

---
+++ WebBanHangOnline/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Admin/Post
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var items = db.Posts.ToList();
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Posts model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CategoryId = 3;
+                model.CreatedDate = DateTime.Now;
+                model.ModifierDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Posts.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Posts.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Posts model)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                model.ModifierDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Posts.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+
+            var item = db.Posts.Find(id);
+            if (item != null)
+            {
+                db.Posts.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+
+            var item = db.Posts.Find(id);
+            if (item != null)
+            {
+                item.IsActive = !item.IsActive;
+                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, isActive = item.IsActive });
+            }
+            return Json(new { success = false });
+        }
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+
+                    foreach (var item in items)
+                    {
+                        var obj = db.Posts.Find(Convert.ToInt32(item));
+                        db.Posts.Remove(obj);
+                        db.SaveChanges();
+                    }
+
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ WebBanHangOnline/Models/CommonAbstract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    public abstract class CommonAbstract
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifierBy { get; set; }
+        public DateTime ModifierDate { get; set; }
+    }
+}
